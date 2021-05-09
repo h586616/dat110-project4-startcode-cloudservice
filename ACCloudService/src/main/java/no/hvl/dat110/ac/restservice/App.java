cfa499dd80294efd89c5aff5a417e3ccee65cfe1
h586616 <lotte-m97@hotmail.com>
2021-05-09 17:38:05 +0200

    app

--- ACCloudService/src/main/java/no/hvl/dat110/ac/restservice/App.java
+++ ACCloudService/src/main/java/no/hvl/dat110/ac/restservice/App.java
@@ -44,8 +44,53 @@ public class App {
 		});
 		
 		// TODO: implement the routes required for the access control service
-		// as per the HTTP/REST operations describined in the project description
-		
+        // as per the HTTP/REST operations describined in the project description
+
+        post("/accessdevice/log",(req, res)->{
+
+            Gson gson = new Gson();
+            AccessMessage message = gson.fromJson(req.body(), AccessMessage.class);
+            int id = accesslog.add(message.getMessage());
+            return gson.toJson(accesslog.get(id));
+
+        });
+
+        get("/accessdevice/log", (req, res) ->  {
+            return accesslog.toJson();
+        });
+
+        get("/accessdevice/log/:id", (req, res) -> {
+
+            Gson gson = new Gson();
+            int id = Integer.parseInt(req.params(":id"));
+            
+           //AccessEntry entry = accesslog.get(id);
+
+            return gson.toJson(accesslog.get(id));
+        });
+
+        put("/accessdevice/code", (req, res) -> {
+
+            Gson gson = new Gson();
+
+            AccessCode code = gson.fromJson(req.body(), AccessCode.class);
+            accesscode.setAccesscode(code.getAccesscode());
+
+            return gson.toJson(code);
+        });
+
+        get("/accessdevice/code", (req, res) -> { // riktig
+            Gson gson = new Gson();
+
+            return gson.toJson(accesscode);
+        });
+
+        delete("/accessdevice/log", (req, res) -> {
+            accesslog.clear();
+            return accesslog.toJson();
+        });
     }
-    
-}
+
+
+
+    }
