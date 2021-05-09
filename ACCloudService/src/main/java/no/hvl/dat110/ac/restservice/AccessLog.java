9344f3f987a3695e27ca45ea2cfa05b5baa21715
h586616 <lotte-m97@hotmail.com>
2021-05-09 17:37:09 +0200

    accesslog

--- ACCloudService/src/main/java/no/hvl/dat110/ac/restservice/AccessLog.java
+++ ACCloudService/src/main/java/no/hvl/dat110/ac/restservice/AccessLog.java
@@ -6,41 +6,43 @@ import java.util.concurrent.atomic.AtomicInteger;
 import com.google.gson.Gson;
 
 public class AccessLog {
-	
-	// atomic integer used to obtain identifiers for each access entry
-	private AtomicInteger cid;
-	protected ConcurrentHashMap<Integer, AccessEntry> log;
-	
-	public AccessLog () {
-		this.log = new ConcurrentHashMap<Integer,AccessEntry>();
-		cid = new AtomicInteger(0);
-	}
 
-	// TODO: add an access entry to the log for the provided message and return assigned id
-	public int add(String message) {
-		
-		int id = 0;
-		
-		return id;
-	}
-		
-	// TODO: retrieve a specific access entry from the log
-	public AccessEntry get(int id) {
-		
-		return null;
-		
-	}
-	
-	// TODO: clear the access entry log
-	public void clear() {
-		
-	}
-	
-	// TODO: return JSON representation of the access log
-	public String toJson () {
-    	
-		String json = null;
-    	
-    	return json;
+    // atomic integer used to obtain identifiers for each access entry
+    private AtomicInteger cid;
+    protected ConcurrentHashMap<Integer, AccessEntry> log;
+
+    public AccessLog () {
+        this.log = new ConcurrentHashMap<Integer,AccessEntry>();
+        cid = new AtomicInteger(0);
+    }
+
+    // TODO: add an access entry to the log for the provided message and return assigned id
+    public int add(String message) {
+
+        AccessEntry entry = new AccessEntry(cid.get(), message);
+        log.put(cid.get(), entry);
+
+        return cid.getAndIncrement();
+    }
+
+    // TODO: retrieve a specific access entry from the log
+    public AccessEntry get(int id) {
+
+        return log.get(id);
+
+    }
+
+    // TODO: clear the access entry log
+    public void clear() {
+
+        log.clear();
+    }
+
+    // TODO: return JSON representation of the access log
+    public String toJson () {
+        Gson gson = new Gson();
+
+
+        return gson.toJson(log);
     }
 }
